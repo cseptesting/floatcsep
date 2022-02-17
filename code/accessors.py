fa6ee9abcad20fab80147acddc392199ee916d0f
pciturri <pciturri@gfz-potsdam.de>
2022-02-17 20:34:52 +0100

    Created ISC gCMT catalog reader

--- code/accessors.py
+++ code/accessors.py
@@ -1,4 +1,31 @@
-def query_gcmt(*args, **kwargs):
+from datetime import datetime
+from urllib import request
+from urllib.parse import urlencode
+from csep.utils.time_utils import datetime_to_utc_epoch
+from csep.core.catalogs import CSEPCatalog
+import xml.etree.ElementTree as ET
+
+HOST = "http://www.isc.ac.uk/cgi-bin/web-db-run?"
+TIMEOUT = 120
+
+def query_isc_gcmt(out_format='QuakeML',
+                     request='COMPREHENSIVE',
+                     searchshape='GLOBAL',
+                     start_year=2020,
+                     start_month=1,
+                     start_day=1,
+                     start_time='00:00:00',
+                     end_year=2022,
+                     end_month=1,
+                     end_day=1,
+                     end_time='00:00:00',
+                     host=None,
+                     include_magnitudes='on',
+                     min_mag=5.95,
+                     req_mag_type='MW',
+                     req_mag_agcy='GCMT',
+                     ):
+
     """ Return gCMT catalog from ISC online web-portal
 
         Args: 
@@ -7,7 +34,78 @@ def query_gcmt(*args, **kwargs):
         Returns:
             out (csep.core.catalogs.AbstractBaseCatalog): gCMT catalog
     """
-    pass
+
+    inputargs = locals().copy()
+    query_args = {}
+    for key, value in inputargs.items():
+        if value is True:
+            query_args[key] = 'true'
+            continue
+        if value is False:
+            query_args[key] = 'false'
+            continue
+        if value is None:
+            continue
+        query_args[key] = value
+
+    events, creation_time = _search_isc_gcmt(**query_args)
+
+    return CSEPCatalog(data=events, date_accessed=creation_time)
+
+
+def _search_isc_gcmt(**newargs):
+
+    """
+    Performs de query at ISC API and returns event list and access date
+
+    """
+    paramstr = urlencode(newargs)
+    url = HOST + paramstr
+    try:
+        fh = request.urlopen(url, timeout=TIMEOUT)
+        data = fh.read().decode('utf8')
+        fh.close()
+        root = ET.fromstring(data)
+        ns = root.tag.split('}quakeml')[0] + '}'
+        creation_time = root[0].find(ns + 'creationInfo').find(ns + 'creationTime').text
+        events_quakeml = root[0].findall(ns + 'event')
+        events = []
+        for feature in events_quakeml:
+            events.append(_parse_isc_event(feature, ns))
+
+    except Exception as msg:
+        raise Exception(
+            'Error downloading data from url %s.  "%s".' % (url, msg))
+
+    return events, creation_time
+
+
+def _parse_isc_event(node, ns, mag_author='GCMT'):
+    """
+    Parse event list from quakeML returned from ISC
+    """
+    id_ = node.get('publicID').split('=')[-1]
+    magnitudes = node.findall(ns + 'magnitude')
+    mag_gcmt = [i for i in magnitudes if i.find(ns + 'creationInfo')[0].text == mag_author][0]
+
+    origin_id = mag_gcmt.find(ns + 'originID').text
+    origins = node.findall(ns + 'origin')
+
+    origin_gcmt = [i for i in origins if i.attrib['publicID'] == origin_id][0]
+
+    lat = origin_gcmt.find(ns + 'latitude').find(ns + 'value').text
+    lon = origin_gcmt.find(ns + 'longitude').find(ns + 'value').text
+    mag = mag_gcmt.find(ns + 'mag').find(ns + 'value').text
+    depth = origin_gcmt.find(ns + 'depth').find(ns + 'value').text
+
+    dtstr = origin_gcmt.find(ns + 'time').find(ns + 'value').text
+    date = dtstr.split('T')[0]
+    time = dtstr.split('T')[1][:-1]
+    dtime = datetime_to_utc_epoch(datetime.fromisoformat(date + ' ' + time + '0'))
+
+    return (id_, dtime, float(lat), float(lon), float(depth) / 1000., float(mag))
+
+
 
 def download_from_zenodo(resource_id):
     """ Downloads file from Zenodo and returns checksum for each file
@@ -21,4 +119,12 @@ def download_from_zenodo(resource_id):
             checksum (string): md5 checksum from model
     """
     pass
+
+if __name__ == '__main__':
+
+    import matplotlib.pyplot as plt
+    cat = query_isc_gcmt(start_year=2018, start_month=8)
+    cat.plot(set_global=True)
+    plt.show()
+
 
