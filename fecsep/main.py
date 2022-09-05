d599b1721034ca476c2b1b28efebd88f50d5db4f
pciturri <pciturri@gfz-potsdam.de>
2022-09-06 00:43:04 +0200

    style: reordered steps. added dirty func to read results > need to implemente something more elegant for self-disc

--- fecsep/main.py
+++ fecsep/main.py
@@ -1,50 +1,48 @@
 import sys
 import datetime
-import os
-from fecsep import models
+from fecsep import core
 from collections import defaultdict
 
 
-def run(cfg, test_date_string):
+def run(cfg, test_date_string, rerun=False):
 
-    exp = models.Experiment.from_yaml(cfg)
-    exp.set_tests()
-    exp.set_models()
-    exp.stage_models()
+
+    exp = core.Experiment.from_yaml(cfg)
     try:
         test_date = datetime.datetime.strptime(test_date_string, '%Y-%m-%dT%H:%M:%S')
     except:
         raise RuntimeError("Error parsing test date string. Should have format='%Y-%m-%dT%H:%M:%S")
     exp.set_test_date(test_date)
-    # exp.models[0].create_forecast(exp.start_date, test_date).plot(plot_args={'filename':
-    #                                                                          'forecast'})
 
     print('Experiment is configured with the following parameters')
     print('======================================================\n')
     print(exp.to_yaml())
 
-    # 0. create expected paths based on configuration
+    exp.set_tests()
+
+    exp.set_models()
+
+    exp.stage_models()
+
     exp.get_run_struct()
 
-    # 1. retrieve data from isc catalog reader or from previous run
-    catalog = exp.get_catalog()
-    if not os.path.exists(exp.target_paths['catalog']):
-        catalog.write_json(exp.target_paths['catalog'])
+    exp.get_catalog()
 
-    # 2. Prepare each test that needs to be computed, could be oom problem for large sized tests
     test_list = exp.prepare_all_tests()
+    if rerun is False:
+        run_results = defaultdict(list)
+        for test in test_list:
+            result = exp.run_test(test)
+            run_results[test.name].append(result)
+    else:                                            #todo: need more elegant way of self-discovery
+        run_results = defaultdict(list)
+        for test in test_list:
+            run_results[test.name] = exp.read_evaluation_result(test, exp.models, exp.target_paths)
 
-    # 3. Evaluate models
-    run_results = defaultdict(list)
-    for test in test_list:
-        result = exp.run_test(test)
-        run_results[test.name].append(result)
-
-    # 4. Plot results
     exp.plot_results(run_results)
 
-    # 5. Generate report
     exp.generate_report()
+
 
 def fecsep():
     """
@@ -55,6 +53,5 @@ def fecsep():
         func = globals()[args[0]]
     except AttributeError:
         raise AttributeError('Function not implemented')
-
     func(*args[1:])
 
