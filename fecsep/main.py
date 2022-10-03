54430e8f16a86f1fc64ddf2946eb1f974671f15e
pciturri <pciturri@gfz-potsdam.de>
2022-10-03 21:44:41 +0200

    feat: uses argparse for terminal args. Just the config path is needed, whereas test_date and used_saved_results are optional, and defaulted to end_date and False.

--- fecsep/main.py
+++ fecsep/main.py
@@ -2,59 +2,65 @@ import sys
 import datetime
 from fecsep import core
 from collections import defaultdict
+import argparse
+import os
 
 
-def run(cfg, test_date_string=None, rerun=False):
-
-
-    exp = core.Experiment.from_yaml(cfg)
-    if test_date_string:
-        try:
-            test_date = datetime.datetime.strptime(test_date_string, '%Y-%m-%dT%H:%M:%S')
-        except:
-            raise RuntimeError("Error parsing test date string. Should have format='%Y-%m-%dT%H:%M:%S")
-        exp.set_test_date(test_date)
-    else:
-        exp.set_test_date(exp.end_date)
-
-    print('Experiment is configured with the following parameters')
-    print('======================================================\n')
-    print(exp.to_yaml())
+def run(config, test_date=None, use_saved=False):
+    exp = core.Experiment.from_yaml(config)
+    exp.set_test_date(test_date)
 
     exp.set_tests()
-
     exp.set_models()
-
+    exp.prepare_paths()
+    print('Experiment is configured with the following parameters')
+    print('======================================================\n')
+    print(exp.target_paths)
+    print(exp.exists)
+    exp.prepare_tasks()
     exp.stage_models()
 
     exp.get_run_struct()
-
+    #
     exp.get_catalog()
 
     test_list = exp.prepare_all_tests()
-    if rerun is False:
+    if use_saved is False:
         run_results = defaultdict(list)
         for test in test_list:
             result = exp.run_test(test)
             run_results[test.name].append(result)
-    else:                                            #todo: need more elegant way of self-discovery
+    else:  # todo: need more elegant way of self-discovery
         run_results = defaultdict(list)
         for test in test_list:
             run_results[test.name] = exp.read_evaluation_result(test, exp.models, exp.target_paths)
 
     exp.plot_results(run_results)
 
-    # exp.generate_report()
+    exp.generate_report()
 
 
 def fecsep():
     """
 
     """
-    args = sys.argv[1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('func', type=str, choices=['run'], help='Run a calculation')
+    parser.add_argument('config', type=str, help='Experiment Configuration file')
+    parser.add_argument('-t', '--test_date', type=str, help='Date to test', default=None)
+    parser.add_argument('-s', '--use_saved', type=str, help='Use saved results', default=False)
+
+    args = parser.parse_args()
     try:
-        func = globals()[args[0]]
+        func = globals()[args.func]
+        args.__delattr__('func')
     except AttributeError:
         raise AttributeError('Function not implemented')
-    func(*args[1:])
+    func(**vars(args))
 
+
+if __name__ == '__main__':
+
+    test_run = '../tests/data_tests/gefe_qtree/'
+    os.chdir(test_run)
+    run('config.yml')
