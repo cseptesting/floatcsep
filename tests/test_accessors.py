3253c1ec5555b513b2af2088bf5c93d9a8339310
pciturri <pciturri@gfz-potsdam.de>
2022-11-17 02:21:39 +0100

    tests: Added test to time/region config readers, along with secondary functions style: pep test_Accessors

--- tests/test_accessors.py
+++ tests/test_accessors.py
@@ -1,10 +1,10 @@
 import os.path
 import vcr
-from matplotlib import pyplot
 from datetime import datetime
-from fecsep.accessors import query_isc_gcmt, _query_isc_gcmt, from_zenodo, from_git, _check_hash
+from fecsep.accessors import query_isc_gcmt, _query_isc_gcmt, from_zenodo, \
+    from_git, _check_hash
 import unittest
-import mock
+from unittest import mock
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -23,9 +23,10 @@ def test_isc_gcmt_search():
     tape_file = os.path.join(isc_gcmt_dir(), 'vcr_search.yaml')
     with vcr.use_cassette(tape_file):
         # Maule, Chile
-        eventlist = _query_isc_gcmt(start_year=2010, start_month=2, start_day=26,
-                                    end_year=2010, end_month=2, end_day=28,
-                                    min_mag=8.5)[0]
+        eventlist = \
+        _query_isc_gcmt(start_year=2010, start_month=2, start_day=26,
+                        end_year=2010, end_month=2, end_day=28,
+                        min_mag=8.5)[0]
         event = eventlist[0]
         assert event[0] == '14340585'
 
@@ -33,14 +34,16 @@ def test_isc_gcmt_search():
 def test_isc_gcmt_summary():
     tape_file = os.path.join(isc_gcmt_dir(), 'vcr_summary.yaml')
     with vcr.use_cassette(tape_file):
-        eventlist = _query_isc_gcmt(start_year=2010, start_month=2, start_day=26,
-                                    end_year=2010, end_month=2, end_day=28,
-                                    min_mag=8.5)[0]
+        eventlist = \
+        _query_isc_gcmt(start_year=2010, start_month=2, start_day=26,
+                        end_year=2010, end_month=2, end_day=28,
+                        min_mag=8.5)[0]
         event = eventlist[0]
         cmp = "('14340585', 1267252513600, -35.98, -73.15, 23.2, 8.78)"
         assert str(event) == cmp
         assert event[0] == '14340585'
-        assert datetime.fromtimestamp(event[1] / 1000.) == datetime.fromtimestamp(1267252513.600)
+        assert datetime.fromtimestamp(
+            event[1] / 1000.) == datetime.fromtimestamp(1267252513.600)
         assert event[2] == -35.98
         assert event[3] == -73.15
         assert event[4] == 23.2
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,161 @@
-pass
+import os.path
+from datetime import datetime
+
+from fecsep.utils import parse_timedelta_string, time_windows_ti, \
+    read_time_config, read_region_config, parse_csep_func
+import unittest
+import fecsep
+import csep
+import numpy
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+class CsepFunctionTest(unittest.TestCase):
+
+    def test_parse_csep_func(self):
+        self.assertIsInstance(parse_csep_func('load_gridded_forecast'),
+                              csep.load_gridded_forecast.__class__)
+        self.assertIsInstance(parse_csep_func('join_struct_arrays'),
+                              csep.utils.join_struct_arrays.__class__)
+        self.assertIsInstance(parse_csep_func('plot_poisson_consistency_test'),
+                              csep.utils.plots.plot_poisson_consistency_test.__class__)
+        self.assertIsInstance(parse_csep_func('italy_csep_region'),
+                              csep.core.regions.italy_csep_region.__class__)
+        self.assertIsInstance(parse_csep_func('plot_forecast_lowres'),
+                              fecsep.utils.plot_forecast_lowres.__class__)
+        self.assertIsInstance(parse_csep_func('from_zenodo'),
+                              fecsep.accessors.from_zenodo.__class__)
+        self.assertIsInstance(parse_csep_func('from_zenodo'),
+                              fecsep.evaluations.vector_poisson_t_w_test.__class__)
+        self.assertRaises(AttributeError, parse_csep_func, 'panic_button')
+
+
+class TimeUtilsTest(unittest.TestCase):
+
+    def test_parse_time_window(self):
+        dt = '1Year'
+        self.assertEqual(parse_timedelta_string(dt), '1-years')
+        dt = '7-Days'
+        self.assertEqual(parse_timedelta_string(dt), '7-days')
+        dt = '1- mOnThS'
+        self.assertEqual(parse_timedelta_string(dt), '1-months')
+        dt = '20        days'
+        self.assertEqual(parse_timedelta_string(dt), '20-days')
+        dt = '1decade'
+        self.assertRaises(ValueError, parse_timedelta_string, dt)
+
+    def test_time_windows_ti(self):
+        start = datetime(2014, 1, 1)
+        end = datetime(2022, 1, 1)
+
+        self.assertEqual(time_windows_ti(start_date=start,
+                                         end_date=end), [(start, end)])
+
+        t1 = [(datetime(2014, 1, 1), datetime(2018, 1, 1)),
+              (datetime(2018, 1, 1), datetime(2022, 1, 1))]
+        self.assertEqual(time_windows_ti(start_date=start,
+                                         end_date=end,
+                                         intervals=2), t1)
+        self.assertEqual(time_windows_ti(start_date=start,
+                                         end_date=end,
+                                         horizon='4-years'), t1)
+        self.assertEqual(time_windows_ti(start_date=start,
+                                         intervals=2,
+                                         horizon='4-years'), t1)
+
+        t2 = [(datetime(2014, 1, 1, 0, 0),
+               datetime(2015, 2, 22, 10, 17, 8, 571428)),
+              (datetime(2015, 2, 22, 10, 17, 8, 571428),
+               datetime(2016, 4, 14, 20, 34, 17, 142857)),
+              (datetime(2016, 4, 14, 20, 34, 17, 142857),
+               datetime(2017, 6, 6, 6, 51, 25, 714285)),
+              (datetime(2017, 6, 6, 6, 51, 25, 714285),
+               datetime(2018, 7, 28, 17, 8, 34, 285714)),
+              (datetime(2018, 7, 28, 17, 8, 34, 285714),
+               datetime(2019, 9, 19, 3, 25, 42, 857142)),
+              (datetime(2019, 9, 19, 3, 25, 42, 857142),
+               datetime(2020, 11, 9, 13, 42, 51, 428571)),
+              (datetime(2020, 11, 9, 13, 42, 51, 428571),
+               datetime(2022, 1, 1, 0, 0))]
+        self.assertEqual(time_windows_ti(start_date=start,
+                                         end_date=end,
+                                         intervals=7), t2)
+
+    def test_time_windows_td(self):
+        pass
+
+    def test_read_time_config(self):
+        start = datetime(2014, 1, 1)
+        end = datetime(2022, 1, 1)
+        intervals = 2
+        config = {'start_date': start,
+                  'end_date': end,
+                  'intervals': intervals}
+        self.assertEqual(read_time_config(None, **config),
+                         read_time_config(config))
+
+        short_config = {'start_date': start,
+                        'end_date': end}
+        time_config = {'intervals': 2}
+        full_config = {'start_date': start,
+                       'end_date': end,
+                       'intervals': 2}
+        self.assertEqual(read_time_config(time_config, **short_config),
+                         read_time_config(None, **full_config))
+
+
+class RegionUtilsTest(unittest.TestCase):
+
+    def test_magnitudes_depth(self):
+        magnitudes = numpy.array([1, 1.1, 1.2])
+        mag_min = 1
+        mag_max = 1.2
+        mag_bin = 0.1
+        depth_max = 1.
+        depth_min = 0.
+
+        config = {'mag_min': mag_min,
+                  'mag_max': mag_max,
+                  'mag_bin': mag_bin,
+                  'depth_min': depth_min,
+                  'depth_max': depth_max}
+
+        region_config = read_region_config(config)
+        self.assertEqual(8, len(region_config))
+        numpy.testing.assert_equal(magnitudes,
+                                   region_config['magnitudes'])
+        numpy.testing.assert_equal(numpy.array([depth_min, depth_max]),
+                                   region_config['depths'])
+
+    def test_region(self):
+        region_origins = numpy.array([[1, 1],
+                                      [2, 1],
+                                      [2, 2],
+                                      [1, 2]])
+        region_path = os.path.join(os.path.dirname(__file__), 'artifacts',
+                                   'regions', 'mock_region')
+        config = {'region': region_path,
+                  'mag_min': 1,
+                  'mag_max': 1.2,
+                  'mag_bin': 0.1,
+                  'depth_min': 0,
+                  'depth_max': 1}
+
+        region_config = read_region_config(config)
+        self.assertEqual(8, len(region_config))
+        numpy.testing.assert_equal(region_origins,
+                                   region_config['region'].origins())
+
+        config = {'region': 'italy_csep_region',
+                  'mag_min': 1,
+                  'mag_max': 1.2,
+                  'mag_bin': 0.1,
+                  'depth_min': 0,
+                  'depth_max': 1}
+        region_path = os.path.join(os.path.dirname(__file__), 'artifacts',
+                                   'regions', 'italy_midpoints')
+        midpoints = numpy.genfromtxt(region_path)
+        region_config = read_region_config(config)
+        numpy.testing.assert_almost_equal(midpoints,
+                                          region_config['region'].midpoints())
