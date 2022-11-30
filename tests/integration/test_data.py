55117f36a1be1bb480dbd2247be4e42ac7628942
pciturri <pciturri@gfz-potsdam.de>
2022-11-30 21:27:43 +0100

    tests: added integration tests (data quality tests) for total of 5 cases: should get most of the possible combinations of region, catalog, query, model typologies, etc.

---
+++ tests/integration/test_data.py
@@ -0,0 +1,48 @@
+from fecsep import main
+import unittest
+import os
+
+
+class DataTest(unittest.TestCase):
+
+    @staticmethod
+    def get_path(case):
+        return os.path.abspath(
+            os.path.join(os.path.dirname(__file__),
+                         f'case_{case}',
+                         f'config.yml')
+        )
+
+    def run_evaluation(self, cfg_file):
+        main.run(cfg_file, show=False)
+
+    def get_eval_dist(self):
+        pass
+
+
+class TimeIndependentTest(DataTest):
+
+    def test_case_a(self):
+        cfg = self.get_path('a')
+        self.run_evaluation(cfg)
+        self.assertEqual(1, 1)
+
+    def test_case_b(self):
+        cfg = self.get_path('b')
+        self.run_evaluation(cfg)
+        self.assertEqual(1, 1)
+
+    def test_case_c(self):
+        cfg = self.get_path('c')
+        self.run_evaluation(cfg)
+        self.assertEqual(1, 1)
+
+    def test_case_d(self):
+        cfg = self.get_path('d')
+        self.run_evaluation(cfg)
+        self.assertEqual(1, 1)
+
+    def test_case_e(self):
+        cfg = self.get_path('e')
+        self.run_evaluation(cfg)
+        self.assertEqual(1, 1)
