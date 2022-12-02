1d4691bbd2645cbf3b611264bd6b37f06809612f
pciturri <pciturri@gfz-potsdam.de>
2022-12-02 15:56:33 +0100

    Refactored Model Class ft: now separated btwn making forecast from file and from code. ft: drop to hdf5 is now an option tests: did multiple test for init (from FS, Zen, Git), setting up the DB if requested, and creating a forecast.

--- fecsep/model.py
+++ fecsep/model.py
@@ -5,16 +5,21 @@ from csep.core.forecasts import GriddedForecast
 from csep.utils.time_utils import decimal_year
 
 from fecsep.accessors import from_zenodo, from_git
-from fecsep.dbparser import load_from_hdf5
+from fecsep.readers import ForecastParsers, HDF5Serializer
 from fecsep.utils import parse_csep_func, timewindow_str
 
 
 class Model:
     def __init__(self, name, path,
                  forecast_unit=1,
-                 authors=None, doi=None,
-                 func=None, func_args=None,
-                 zenodo_id=None, giturl=None, repo_hash=None):
+                 authors=None,
+                 doi=None,
+                 use_db=False,
+                 func=None,
+                 func_kwargs=None,
+                 zenodo_id=None,
+                 giturl=None,
+                 repo_hash=None):
         """
 
         Args:
@@ -48,9 +53,27 @@ class Model:
             (ti - file - local)
             (ti - file - zenodo)
             (ti - file - git)
-            
+                (ti - code)  >>> TBI, but probably out of the scope.
+                
             (td - code - local)
             (td - code - git)
+            (td - code - zenodo)
+            
+        Get forecasts:
+            - FILE
+                A - read from file, scale in runtime
+                B - drop to db, scale from function in runtime   (only makes sense to speed things)
+                C - drop to db, scale and drop to db
+            - SOURCE
+                D - run, read from file              (D similar to A) 
+                E - run, store in db, read from db   (E similar to C)
+    
+        OPTIONS:
+            - TI <-> FROM FILE     - SCALE IN RT
+                                   - SCALE and drop to DB
+            - TD <-> FROM FILE     - TO DB 
+                     FROM SRC      - TO DB
+                                   
         '''
 
         # todo list
@@ -58,30 +81,52 @@ class Model:
         #  - Instantiate from source code
         #  - Check contents when instantiating from_git
 
+        # INIT ATTRIBUTES
         self.name = name
-        self.path = path
-        self._dir = None
-        self.dbpath = None
-
         self.zenodo_id = zenodo_id
         self.giturl = giturl
         self.repo_hash = repo_hash
-
-        self.format = None
-        self.get_source(zenodo_id, giturl)
-
-        if self.format != 'src':
-            self.dbserializer = parse_csep_func(self.format)
-            self.make_db()
-
         self.authors = authors
         self.doi = doi
-
         self.forecast_unit = forecast_unit
-
-        self.forecasts = {}
         self.func = func
-        self.func_args = func_args
+        self.func_args = func_kwargs
+        self.use_db = use_db
+
+        # SET MODEL CLASS
+        if self.func:
+            self._class = 'td'  # Time-Dependent todo: implement for TI
+        else:
+            self._class = 'ti'  # Time-Independent
+
+        # PATHS # todo: get from registry
+        self._path = path
+        self._dir = None
+        self.dbpath = None
+
+        # MODEL SELF-DISCOVER ATTRS
+        self._src = None
+        self._fmt = None
+        self.get_source(zenodo_id, giturl)
+
+        if self.use_db:
+            if self._class == 'ti':
+                self.db_func = HDF5Serializer.grid2hdf5
+                self.make_db()
+            else:
+                raise NotImplementedError('missing for Time-Dep')
+        else:
+            self.db_func = None
+
+        # INSTANTIATE ATTRIBUTES TO BE POPULATED IN runtime
+        self.forecasts = {}
+
+    @property
+    def path(self):
+        if self.dbpath:
+            return self.dbpath
+        else:
+            return self._path
 
     def get_source(self, zenodo_id=None, giturl=None, **kwargs):
         """
@@ -104,26 +149,27 @@ class Model:
         # Check if the provided path is a file or dir.
         ext = os.path.splitext(self.path)[-1]
 
-        if bool(ext):
-            self._dir = os.path.dirname(self.path)
-            self.format = ext.split('.')[-1]
-        else:
-            self._dir = self.path
-            self.format = 'src'
+        if bool(ext):  # model is a file
+            self._dir = os.path.dirname(self.path)  # todo reg
+            self._fmt = ext.split('.')[-1]
+            self._src = 'file'
+        else:  # model is bin
+            self._dir = self.path  # todo reg
+            self._src = 'bin'
 
-        if not os.path.exists(self.path):
-            # It does not exist, get from zenodo or git
+        # Folder nor file exists -> get from zenodo or git
+        if not os.path.exists(self.path):  # todo reg
             if zenodo_id is None and giturl is None:
                 raise FileNotFoundError(
-                    f"Model file or directory '{self.path}' not found")
-            # Model needs to be downloaded from zenodo/git
+                    f"Model file or directory '{self.path}' not found")  # todo reg
+
             os.makedirs(self._dir, exist_ok=True)
             try:
                 # Zenodo is the first source of retrieval
-                from_zenodo(zenodo_id, self._dir, **kwargs)
+                from_zenodo(zenodo_id, self._dir, **kwargs)  # todo reg
             except KeyError or TypeError as zerror_:
                 try:
-                    from_git(giturl, self._dir, **kwargs)
+                    from_git(giturl, self._dir, **kwargs)  # todo reg
                 except (git.NoSuchPathError, git.CommandError) as giterror_:
                     if giturl is None:
                         raise KeyError('Zenodo identifier is not valid')
@@ -132,32 +178,31 @@ class Model:
 
             # Check if file or directory exists after downloading
             if bool(ext):
-                path_exists = os.path.isfile(self.path)
+                path_exists = os.path.isfile(self.path)  # todo reg
             else:
-                path_exists = os.path.isdir(self.path)
+                path_exists = os.path.isdir(self.path)  # todo reg
 
             assert path_exists
 
-    def make_db(self):
+    def make_db(self, force=False):
         """
 
         Returns:
 
         """
 
-        self.dbpath = os.path.splitext(self.path)[0] + '.hdf5'
-
-        if not os.path.isfile(self.dbpath):
-            self.dbserializer(self.path, self.dbpath)
+        dbpath = os.path.splitext(self.path)[0] + '.hdf5'  # todo: reg
+        if not os.path.isfile(dbpath) or force:
+            self.db_func(self.path, self._fmt, dbpath)  # todo: reg
+        self.dbpath = dbpath
+        self._fmt = 'hdf5'
 
     def rm_db(self):
 
         if os.path.isfile(self.dbpath):
             os.remove(self.dbpath)
-            return True
         else:
             print("The HDF5 file does not exist")
-            return False
 
     def create_forecast(self, start_date, end_date, **kwargs):
         """
@@ -167,30 +212,30 @@ class Model:
         :return: A pycsep.core.forecasts.GriddedForecast object
         """
 
-        if self.path == self._dir:
-            # Forecasts are created from source code
-            self.make_forecast_td(start_date, end_date, **kwargs)
+        if self._src == 'file':
+            self.forecast_from_file(start_date, end_date, **kwargs)
+
         else:
             # Forecasts are created from file
             self.make_forecast_ti(start_date, end_date, **kwargs)
 
-    def make_forecast_td(self, start_date, end_date, **kwargs):
+    def forecast_from_func(self, start_date, end_date, **kwargs):
         pass
 
-    def make_forecast_ti(self, start_date, end_date, **kwargs):
+    def forecast_from_file(self, start_date, end_date, **kwargs):
 
-        time_horizon = decimal_year(end_date) - decimal_year(start_date)
-        tstring = timewindow_str([start_date, end_date])
+        time_horizon = decimal_year(end_date) - \
+                       decimal_year(start_date)
+        tstring = timewindow_str([start_date, end_date])  # reg
 
-        # todo implement these functions in dbparser
-
-        rates, region, magnitudes = load_from_hdf5(self.dbpath)
+        f_parser = getattr(ForecastParsers, self._fmt)
+        rates, region, mags = f_parser(self.path)
 
         forecast = GriddedForecast(
             name=f'{self.name}',
             data=rates,
             region=region,
-            magnitudes=magnitudes,
+            magnitudes=mags,
             start_time=start_date,
             end_time=end_date
         )
@@ -199,7 +244,9 @@ class Model:
         print(
             f"Forecast expected count: {forecast.event_count:.2f}"
             f" with scaling parameter: {time_horizon:.1f}")
-        self.forecasts[tstring] = forecast
+
+        self.forecasts[
+            tstring] = forecast  # todo option to keep in memory or drop
 
     def to_dict(self):
         # todo: modify this function to include more state from the class
@@ -210,9 +257,16 @@ class Model:
                 out[k] = v
         return out
 
+    def get_forecast(self, start, end):
+        """ Wrapper that just returns a forecast,
+         hiding the processing (db storage, ti_td, etc, etc) under the hood"""
+        pass
+
     @classmethod
     def from_dict(cls, record):
-        if len(record) != 1:
+        if 'name' in record.keys():
+            return cls(**record)
+        elif len(record) != 1:
             raise IndexError('A single model has not been passed')
         name = next(iter(record))
         return cls(name=name, **record[name])
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,11 +1,10 @@
 import os.path
 import tempfile
 from datetime import datetime
-from csep.utils.time_utils import decimal_year
 
 import numpy.testing
 
-from fecsep.utils import timewindow_str
+from fecsep import readers
 from unittest import TestCase
 import filecmp
 
@@ -14,23 +13,59 @@ import shutil
 
 
 class TestModel(TestCase):
-    _path = os.path.dirname(__file__)
 
-    def assertEqualModel(self, exp_a, exp_b):
-        pass
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls._path = os.path.dirname(__file__)
+        cls._dir = os.path.join(cls._path, 'artifacts', 'models')
+        cls._alm_fn = os.path.join(
+                               cls._path, '../../examples',
+                               'case_e',
+                               'models',
+                               'gulia-wiemer.ALM.italy.10yr.2010-01-01.xml'
+                                   )
 
-    def test_init_from_filesystem(self):
+    @staticmethod
+    def assertEqualModel(exp_a, exp_b):
+
+        keys_a = list(exp_a.__dict__.keys())
+        keys_b = list(exp_a.__dict__.keys())
+
+        if keys_a != keys_b:
+            raise AssertionError('Models are not equal')
+
+        for i in keys_a:
+            if not (getattr(exp_a, i) == getattr(exp_b, i)):
+                raise AssertionError('Models are not equal')
+
+    def test_from_filesystem(self):
+        """ init from file, check attributes"""
         name = 'mock'
-        path = os.path.join(TestModel._path, 'artifacts',
-                            'models', 'model.csv')
-        model = Model(name, path)
+        fname = os.path.join(self._dir, 'model.csv')
+        model = Model(name, fname, use_db=False)
         self.assertEqual(name, model.name)
-        self.assertEqual(path, model.path)
-        self.assertEqual('csv', model.format)
-        self.assertEqual(os.path.dirname(path), model._dir)
-        self.assertTrue(model.rm_db())
+        self.assertEqual(fname, model.path)
+        self.assertEqual('ti', model._class)
+        self.assertEqual('csv', model._fmt)
+        self.assertEqual('file', model._src)
+        self.assertEqual(self._dir, model._dir)
+        self.assertEqual(1, model.forecast_unit)
 
-    def test_init_from_zenodo(self):
+        self.assertIs(None, model.db_func)
+
+    def test_from_filesystem_DB(self):
+        """ init from file, check for hdf5 db atrrs """
+        name = 'mock'
+        fname = os.path.join(self._dir, 'model.csv')
+        model = Model(name, fname, use_db=True)
+
+        self.assertTrue(os.path.isfile(model.dbpath))
+        model.rm_db()
+        self.assertFalse(os.path.isfile(model.dbpath))
+
+    def test_from_zenodo(self):
+        """ downloads model from zenodo """
+
         name = 'mock_zenodo'
         filename_ = 'TEAM=N10L11.csv'
         dir_ = os.path.join(tempfile.tempdir, 'mock')
@@ -38,86 +73,146 @@ class TestModel(TestCase):
             shutil.rmtree(dir_)
         path_ = os.path.join(dir_, filename_)
         zenodo_id = 6289795
+
+        # Initialize from zenodo id
         model_a = Model(name, path_, zenodo_id=zenodo_id)
 
-        path = os.path.join(TestModel._path, 'artifacts', 'models',
+        # Initialize from the files downloaded
+        path = os.path.join(self._path, 'artifacts', 'models',
                             'qtree', filename_)
-        model_b = Model(name, path)
+        model_b = Model(name, path, zenodo_id=6289795)
 
-        self.assertEqual('csv', model_a.format)
-        self.assertEqual('csv', model_b.format)
+        self.assertEqual('csv', model_a._fmt)
+        self.assertEqual('csv', model_b._fmt)
         self.assertEqual(model_a.name, model_b.name)
         self.assertTrue(filecmp.cmp(model_a.path, model_b.path))
-        self.assertTrue(model_a.rm_db())
-        self.assertTrue(model_b.rm_db())
+        self.assertFalse(model_a.dbpath)
 
-    def test_init_from_git(self):
+    def test_from_git(self):
         name = 'mock_git'
         _dir = 'template_'
         path_ = os.path.join(tempfile.tempdir, _dir)
-        giturl = 'https://git.gfz-potsdam.de/csep-group/rise_italy_experiment/' \
-                 'models/template.git'
+        giturl = 'https://git.gfz-potsdam.de/csep-group/' \
+                 'rise_italy_experiment/models/template.git'
         model_a = Model(name, path_, giturl=giturl)
-        path = os.path.join(TestModel._path, 'artifacts', 'models',
-                            'template')
+        path = os.path.join(self._dir, 'template')
         model_b = Model(name, path)
         self.assertEqual(model_a.name, model_b.name)
-        self.assertEqual('src', model_b.format)
+        self.assertEqual('bin', model_b._src)
         dircmp = filecmp.dircmp(model_a.path, model_b.path).common
         self.assertGreater(len(dircmp), 8)
         shutil.rmtree(path_)
 
     def test_from_dict(self):
         name = 'mock'
-        path = os.path.join(TestModel._path, 'artifacts',
-                            'models', 'model.csv')
+        fname = os.path.join(self._dir, 'model.csv')
 
         dict_ = {'mock':
-                     {'path': os.path.join(TestModel._path, 'artifacts',
-                                           'models', 'model.csv'),
-                      'forecast_unit': 5,
-                      'authors': ['Darwin, C.', 'Bell, J.', 'Et, Al.'],
-                      'doi': '10.1010/10101010',
-                      'giturl': 'should not be accessed, bc filesystem exists',
-                      'zenodo_id': 'should not be accessed, bc filesystem exists'
-                      }}
+                 {'path': fname,
+                  'forecast_unit': 5,
+                  'authors': ['Darwin, C.', 'Bell, J.', 'Et, Al.'],
+                  'doi': '10.1010/10101010',
+                  'giturl': 'should not be accessed, bc filesystem exists',
+                  'zenodo_id': 'should not be accessed, bc filesystem exists'
+                  }
+                 }
 
-        model = Model.from_dict(dict_)
-        self.assertEqual(name, model.name)
-        self.assertEqual(path, model.path)
-        self.assertEqual('csv', model.format)
-        self.assertEqual(os.path.dirname(path), model._dir)
-        self.assertTrue(model.rm_db())
+        # Similar to import from YML
+        model_a = Model.from_dict(dict_)
 
-    def test_create_forecast_ti(self):
+        # Import from normal py dict structure
+        py_dict = {'name': 'mock', **dict_['mock']}
+        model_b = Model.from_dict(py_dict)
+
+        self.assertEqual(name, model_a.name)
+        self.assertEqual(fname, model_a.path)
+        self.assertEqual('csv', model_a._fmt)
+        self.assertEqual('file', model_a._src)
+        self.assertEqual(self._dir, model_a._dir)
+
+        self.assertEqualModel(model_a, model_b)
+
+    def test_create_forecasts(self):
+        """ Selects TI or TD appropiately """
+        pass
+
+    def test_forecast_ti_from_file(self):
+        """ reads from file, scale in runtime """
         name = 'mock'
-        path = os.path.join(os.path.dirname(__file__),
-                            'artifacts', 'models', 'model.csv')
-        rates = numpy.array([[1., 0.1],
-                             [1., 0.1],
-                             [1., 0.1],
-                             [1., 0.1]])
+        fname = os.path.join(self._dir, 'model.csv')
+        model = Model(name, fname, use_db=False)
 
-        model = Model(name, path)
+        start = datetime(1900, 1, 1)
+        end = datetime(2000, 1, 1)
+        model.forecast_from_file(start, end)
+
+        numpy.testing.assert_almost_equal(440., model.forecasts[
+            '1900-01-01_2000-01-01'].data.sum())
+
+    def test_forecast_ti_from_xml(self):
+        """ reads from file, scale in runtime """
+        name = 'ALM'
+        fname = self._alm_fn
+
+        numpy.seterr(all="ignore")
+
+        model = Model(name, fname, use_db=False)
+
+        start = datetime(1900, 1, 1)
+        end = datetime(2000, 1, 1)
+        model.forecast_from_file(start, end)
+
+        numpy.testing.assert_almost_equal(1618.5424321406535, model.forecasts[
+            '1900-01-01_2000-01-01'].data.sum())
+
+    def test_forecast_ti_from_xml2hdf5(self):
+        """ reads from file, drops to db, reads from db, scale in runtime """
+        name = 'ALM'
+        fname = self._alm_fn
+
+        numpy.seterr(all="ignore")
+
+        model = Model(name, fname, use_db=True)
+
+        start = datetime(1900, 1, 1)
+        end = datetime(2000, 1, 1)
+        model.forecast_from_file(start, end)
+
+        numpy.testing.assert_almost_equal(1618.5424321406535, model.forecasts[
+            '1900-01-01_2000-01-01'].data.sum())
+
+    def test_forecast_ti_from_db(self):
+        """ reads from file, scale in runtime """
+        name = 'mock'
+        fname = os.path.join(self._dir, 'model.csv')
+        model = Model(name, fname, use_db=True)
+
         start = datetime(2020, 1, 1)
-        end = datetime(2021, 1, 1)
-        model.create_forecast(start, end)
-        tstr = timewindow_str([start, end])
+        end = datetime(2023, 1, 1)
+        model.forecast_from_file(start, end)
+        numpy.testing.assert_almost_equal(13.2, model.forecasts[
+            '2020-01-01_2023-01-01'].data.sum())
 
-        self.assertIn(tstr, model.forecasts)
-        self.assertEqual(f'{name}_{tstr}', model.forecasts[tstr].name)
-        numpy.testing.assert_equal(4.4, model.forecasts[tstr].event_count)
-        numpy.testing.assert_allclose(rates, model.forecasts[tstr].data)
+        assert True
 
-        end = datetime(2020, 3, 1)
-        model.create_forecast(start, end)
-        tstr = timewindow_str([start, end])
+    def test_make_forecast_ti_dbrt(self):
+        """ reads from generated db, scale in runtime """
+        pass
 
-        self.assertIn(tstr, model.forecasts)
-        self.assertEqual(f'{name}_{tstr}', model.forecasts[tstr].name)
-        numpy.testing.assert_almost_equal(0.72131147541031171,
-                                          model.forecasts[tstr].event_count)
-        numpy.testing.assert_allclose(rates * (decimal_year(end) -
-                                               decimal_year(start)),
-                                      model.forecasts[tstr].data)
-        self.assertTrue(model.rm_db())
+    def test_make_forecast_ti_db_fs(self):
+        """ reads from generated db, scale and drops to db """
+        pass
+
+    def test_todict(self):
+        pass
+
+    def test_make_forecast_td(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+
+        alm_db = os.path.join(cls._path, '../../examples', 'case_e',
+                              'models',
+                              'gulia-wiemer.ALM.italy.10yr.2010-01-01.hdf5')
+        os.remove(alm_db)
